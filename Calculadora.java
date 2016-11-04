3e3280eee7a1cdb28252f38ed90a1fb68a7eb1de
MasterJB27 <joshboni2721@gmail.com>
2016-11-04 11:58:16 -0600

    Estructuración del Switch
    Se comienza a estructurar el switch con las opciones que el usuario
    podrá elegir en el programa

--- Calculadora.java
+++ Calculadora.java
@@ -16,7 +16,25 @@ public class Calculadora {
         System.out.println("Ingrese una opcion");
         ing = in.nextInt();
         switch (ing) {
-
+          case 1:
+          System.out.println("Selecciono la opcion \"1. Suma de dos numeros\"");
+          double sum1=0, sum2=0, suma=0, op=0;
+          Scanner n1 = new Scanner(System.in);
+          Scanner n2 = new Scanner(System.in);
+          System.out.println("Ingrese primer numero: ");
+          sum1= n1.nextInt();
+          System.out.println('\n');
+          System.out.println("Ingrese segundo numero: ");
+          sum2= n2.nextInt();
+          suma= sum1+sum2;
+          System.out.println('\n');
+          System.out.println("El resultado de la suma es: "+suma +'\n');
+          System.out.println("Presione una tecla para desplegar nuevamente el Menu Principal");
+          String ingrese1 = " ";
+          Scanner ot1 = new Scanner(System.in);
+          ingrese1 = ot1.nextLine();
+          System.out.println('\n');
+          break;
         }
       }
     }
