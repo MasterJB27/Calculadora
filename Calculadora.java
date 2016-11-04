d883eef281ed9b0bcd60aa35f643556314590e07
MasterJB27 <joshboni2721@gmail.com>
2016-11-04 11:55:28 -0600

    Creación del Menú
    Se inicia el programa creando el menú que nos guiará en el programa

---
+++ Calculadora.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main (String [] args){
+      int ing;
+      Scanner in = new Scanner(System.in);
+      System.out.println("MENU");
+      System.out.println("1. Suma de dos numeros");
+      System.out.println("2. Resta de dos numeros");
+      System.out.println("3. Multiplicacion de dos numeros");
+      System.out.println("4. Division de dos numeros");
+      System.out.println("5. Ver Bitacora");
+      System.out.println("6. Borrar Bitacora");
+      System.out.println("7. Salir");
+        System.out.println("Ingrese una opcion");
+        ing = in.nextInt();
+        switch (ing) {
+
+        }
+      }
+    }
